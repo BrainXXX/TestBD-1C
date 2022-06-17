8bdab9fa36ab498b5511818d812a08904e08f9fc
BrainXXX <xxxbrainxxx@gmail.com>
2022-06-17 14:49:12 +1000

    17.06.2022 14:50

--- Form1.cs
+++ Form1.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Configuration;
+using System.Data;
 using System.Data.SqlClient;
 using System.Windows.Forms;
 
@@ -10,6 +11,7 @@ namespace TestBD_1C
         private SqlConnection _connection = null;
         private SqlDataReader _reader = null;
         private SqlCommand _query = null;
+        private readonly string number = "ПзС-000100";
 
         public Form1()
         {
@@ -39,19 +41,53 @@ namespace TestBD_1C
 	                                                    CONCAT('ЗаказТовара ', Номер, ' от ', CONCAT(CONVERT(VARCHAR, Дата, 104), ' ', CONVERT(VARCHAR, Дата, 108))) AS ЗаказТовара
 	                                                    FROM [dns_m].[dwh].[Документ.ЗаказТовара]
 	                                                    INNER JOIN dns_m.dwh.[Справочник.Фирмы] AS Фирма ON Фирма.Ссылка = [dns_m].[dwh].[Документ.ЗаказТовара].Фирма
-	                                                    WHERE Номер = 'ПзС-000100'", _connection);
+	                                                    WHERE Номер = @number;SET @id=SCOPE_IDENTITY()", _connection);
+
+                        // Задаем значение параметра @number в одну строку
+                        //_query.Parameters.AddWithValue("number", "ПзС-000100");
+                        // создаем параметр для номера документа отдельно
+                        SqlParameter nameParam = new SqlParameter("@number", System.Data.SqlDbType.NVarChar, 12);
+                        // определяем значение
+                        nameParam.Value = number;
+                        // добавляем параметр к команде
+                        _query.Parameters.Add(nameParam);
+
+                        // Выходной параметр для id, работает для INSERT
+                        SqlParameter idParam = new SqlParameter
+                        {
+                            ParameterName = "@id",
+                            SqlDbType = SqlDbType.Int,
+                            Direction = ParameterDirection.Output // параметр выходной
+                        };
+                        _query.Parameters.Add(idParam);
 
                         using (_reader = await _query.ExecuteReaderAsync())
                         {
-                            label1.Text = "";
-
-                            while (await _reader.ReadAsync())
+                            if (_reader.HasRows) // если есть данные
                             {
-                                label1.Text += $"[{_reader["Номер"]}] {_reader["Фирма"]} - {_reader["ЗаказТовара"]}\n";
+                                // Выводим названия колонок ColumnName
+                                label1.Text = $"{_reader.GetName(0),10} {_reader.GetName(1),15} {_reader.GetName(2),50}\n\n";
+
+                                while (await _reader.ReadAsync())
+                                {
+                                    string number = _reader.GetString(0);
+
+                                    label1.Text += $"[{number}] {_reader["Фирма"]} - {_reader["ЗаказТовара"]}\n";
+                                }
+                            }
+                            else
+                            {
+                                label1.Text += $"Таблица пустая!\n";
                             }
                         }
 
-                        label1.Text += $"ClientConnectionId: {_connection.ClientConnectionId}\n";
+                        _query.CommandText = @"SELECT COUNT(*)
+	                                                    FROM [dns_m].[dwh].[Документ.ЗаказТовара]
+	                                                    WHERE Номер = 'ПзС-000100'";
+                        object count = await _query.ExecuteScalarAsync();
+
+                        label1.Text += $"\nВыходной параметр: {idParam.Value}";
+                        label1.Text += $"\nКоличество документов: {count}\n\nClientConnectionId: {_connection.ClientConnectionId} [{DateTime.Now}]\n";
                     }
                 }
                 else
